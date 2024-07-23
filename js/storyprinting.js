d834f0bd818b201ac8638619766da47ed372feb6
Samuel-Urbanetto <60645289+Samuel-Urbanetto@users.noreply.github.com>
2024-07-23 15:47:01 -0300

    Add files via upload

--- js/storyprinting.js
+++ js/storyprinting.js
@@ -13,18 +13,20 @@ const edit = (json) => {
     qs("#story").innerHTML += json.story;
 }
 
+var iframe, iframeP, iframeH1, iframeH2, iframeH3, iframeH4;
+
 const loader = async (id) => {
     const response = await fetch('/stories/story.json');
     const json = await response.json();
     console.log(response);
 
-    if (id > Object.keys(json).length-1 || id < 0) {
+    if (id > Object.keys(json).length - 1 || id < 0) {
         window.location.href = '/story.html';
     }
 
     let correct_json = json.filter((filter) => filter.id == id)[0]
-
-    edit(correct_json)
+    edit(correct_json);
+    iframe = qs("#storyiframe").contentWindow;
 }
 
 const paramURL = window.location.search;
@@ -40,20 +42,18 @@ let id = Number.parseInt(ParsedParam.get("id"))
 loader(id)
 
 const updater = async (int) => {
-    id+=int;
+    id += int;
     loader(id)
     ParsedParam.set("id", id)
-    window.history.replaceState(null, null, "?id="+ParsedParam.get('id'))
+    window.history.replaceState(null, null, "?id=" + ParsedParam.get('id'))
 }
 
 //put iframe in page::end
 
 //visual settings buttons::start
 
-var iframe = document.getElementById("storyiframe").getElementsByClassName("c12");
-
-const settingsButton = (iframe) => {
-    iframe.innerHTML += "<button id=\"settingsButton\"></button>"
+const lighttheme = () => {
+    iframe.document.querySelector("p").style.color = "blue";
+    if (iframe != null) { console.log(true) }
+    else { console.log(false) }
 }
-
-settingsButton()
